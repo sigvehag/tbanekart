b496c668b0fcde1355e4ac4e1a978dbd7b8a4f44
sigvehag <sigve@hagesaeter.com>
2019-01-16 19:59:30 +0100

    Changed XML Parse approach
    Changed XML Parse Approach. Optimized Readability and runtime.
    Includes small dict for demo.

--- Innhenting.py
+++ Innhenting.py
@@ -3,63 +3,43 @@ import json
 import xml.etree.ElementTree as ET
 from datetime import datetime
 
-
-# Make a get request to get the latest position of the international space station from the opennotify api.
 response = requests.get("https://api.entur.org/anshar/1.0/rest/et?datasetId=RUT")
-e = ET.fromstring(response.content)
-# Print the status code of the response.
-# print(e[0][0].tag)
+root = ET.fromstring(response.content)
 
-name = ""
-dest = ""
-expTime = ""
-reqTime = 0
-shouldBreak = False
+quayDict= {
+    **dict.fromkeys(["11117", "11115"] , "Storo"),
+    "11135" : "Helsfyr",
+    "10421" : "Mortensrud",
+    "11411" : "Sognsvann",
+    "7257" : "Stortinget"
+}
 
-for data in e[0][3][1][1:]:
-    for subdata in data:
-        if subdata.tag == "{http://www.siri.org.uk/siri}EstimatedCalls":
-            for estimatedCall in subdata:
-                for metaData in estimatedCall:
-                    shouldBreak = False
-                    if (
-                        metaData.tag
-                        == "{http://www.siri.org.uk/siri}ExpectedDepartureTime"
-                    ):
-                        expTime = metaData.text
-                        for fmt in (
-                            "%Y-%m-%dT%H:%M:%S+01:00",
-                            "%Y-%m-%dT%H:%M:%S.%f+01:00",
-                        ):
-                            try:
-                                reqTime = (
-                                    datetime.strptime(expTime, fmt) - datetime.now()
-                                )
 
-                                if reqTime.total_seconds() > 0:
-                                    for metaData in estimatedCall:
-
-                                        if (
-                                            metaData.tag
-                                            == "{http://www.siri.org.uk/siri}StopPointName"
-                                        ):
-                                            name = metaData.text
-                                    for metaData in estimatedCall:
-                                        if (
-                                            metaData.tag
-                                            == "{http://www.siri.org.uk/siri}DestinationDisplay"
-                                        ):
-                                            dest = metaData.text
-
-                                    print(name, dest, reqTime)
-                                    shouldBreak = True
-                                    
-
-                            except ValueError:
-                                pass
-                    if shouldBreak:
-                        break
-                if shouldBreak:
-                    break        
-        # if shouldBreak:
-        #     break
+trips = root[0][3][1]
+for trip in trips.iter('{http://www.siri.org.uk/siri}EstimatedVehicleJourney'):
+    for stop in trip.iter('{http://www.siri.org.uk/siri}EstimatedCall'):        
+        for data in stop.iter('{http://www.siri.org.uk/siri}ExpectedDepartureTime'):
+            expectedDeparture = data.text
+            
+            willLeaveIn = 0
+            for fmt in ("%Y-%m-%dT%H:%M:%S+01:00", "%Y-%m-%dT%H:%M:%S.%f+01:00"):
+                try:
+                    willLeaveIn = datetime.strptime(expectedDeparture, fmt) - datetime.now()
+                except ValueError:
+                    pass
+            if willLeaveIn.total_seconds() > 0:
+                line ="Kunne ikke finne linje"
+                stopName ="Kunne ikke finne stopp"
+                try:
+                    line = trip.find('{http://www.siri.org.uk/siri}LineRef').text
+                    stopName = stop.find('{http://www.siri.org.uk/siri}StopPointRef').text
+                except AttributeError:
+                    pass
+                
+                try:
+                    stopName = quayDict[stopName[9:]]
+                except KeyError:
+                    pass
+                print(line[9], "will leave", stopName,  "in", willLeaveIn)
+                break
+        break
