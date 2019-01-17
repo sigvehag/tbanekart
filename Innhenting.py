b161a6b602fbcf68539a52201c16d6e4dccaf695
sigvehag <sigve@hagesaeter.com>
2019-01-17 16:09:52 +0100

    Added CSV-reading and fixed the bug where it only got the lines leaving from their start station

--- Innhenting.py
+++ Innhenting.py
@@ -1,8 +1,23 @@
+# -*- coding: utf-8 -*-
 import requests
-import json
 import xml.etree.ElementTree as ET
 from datetime import datetime
 import sched, time
+import csv
+import ast
+
+### Reading stops from csv
+with open('stops.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)                                # Skips Header
+    quayDict = {}
+    for row in reader:
+        fork, name, intID, quay = row
+        try:
+            quay = ast.literal_eval(quay)       # Parses String to list
+            quayDict.update(dict.fromkeys(quay, name))
+        except SyntaxError:
+            pass
 
 ### Timing
 scheduler = sched.scheduler(time.time, time.sleep)
@@ -11,56 +26,47 @@ scheduler = sched.scheduler(time.time, time.sleep)
 response = requests.get("https://api.entur.org/anshar/1.0/rest/et?datasetId=RUT")
 root = ET.fromstring(response.content)
 
-
-### Stops Dictionary
-quayDict= {
-    **dict.fromkeys(["11117", "11115"] , "Storo"),
-    **dict.fromkeys(["10681", "10682"], "Bergkrystallen"),
-    **dict.fromkeys(["6658", "6659"], "Østerås"),
-    **dict.fromkeys(["10421", "10423"], "Mortensrud"),
-    **dict.fromkeys(["10330", "10331"], "Ellingsrudåsen"),
-    "11135" : "Helsfyr",
-    "11411" : "Sognsvann",
-    "7257" : "Stortinget",
-    "10508" : "Vestli",
-    "7319" : "Kolsås",
-    "12150" : "Frognerseteren",
-    "102017": "Løren"
-}
-
 ### Parser
 def parseAndPrint(sch):
     print("--------------------", datetime.now(), "--------------------")
     trips = root[0][3][1]
     for trip in trips.iter('{http://www.siri.org.uk/siri}EstimatedVehicleJourney'):
-        for stop in trip.iter('{http://www.siri.org.uk/siri}EstimatedCall'):        
+        line = "Kunne ikke finne linje"
+        willLeaveIn = 0
+
+        try:
+            line = trip.find('{http://www.siri.org.uk/siri}LineRef').text
+        except AttributeError:
+            pass
+        
+        ### All of one lines stops
+        stops = trip.find('{http://www.siri.org.uk/siri}EstimatedCalls')
+        for stop in stops.iter('{http://www.siri.org.uk/siri}EstimatedCall'):
+            stopName = stop.find('{http://www.siri.org.uk/siri}StopPointRef').text 
+                   
             for data in stop.iter('{http://www.siri.org.uk/siri}ExpectedDepartureTime'):
-                expectedDeparture = data.text
-                
                 willLeaveIn = 0
+                expectedDeparture = data.text                
                 for fmt in ("%Y-%m-%dT%H:%M:%S+01:00", "%Y-%m-%dT%H:%M:%S.%f+01:00"):
                     try:
-                        willLeaveIn = datetime.strptime(expectedDeparture, fmt) - datetime.now()
+                        willLeaveIn = (datetime.strptime(expectedDeparture, fmt) - datetime.now()).total_seconds()
                     except ValueError:
                         pass
-                if willLeaveIn.total_seconds() > 0 and willLeaveIn.total_seconds() < 300:
-                    line ="Kunne ikke finne linje"
-                    stopName ="Kunne ikke finne stopp"
-                    try:
-                        line = trip.find('{http://www.siri.org.uk/siri}LineRef').text
-                        stopName = stop.find('{http://www.siri.org.uk/siri}StopPointRef').text
-                    except AttributeError:
-                        pass
-                    
-                    try:
-                        stopName = quayDict[stopName[9:]]
-                    except KeyError:
-                        pass
-                    print(line[9], "will leave", stopName,  "in", willLeaveIn)
-                    break
-            break
+            
+            ### When the next stop is found it prints the info 
+            ### and breaks out of the current lines loop
+            if willLeaveIn > 0 and willLeaveIn < 300:
+                try:
+                    stopName = quayDict[stopName[9:]]
+                except KeyError:
+                    pass
+
+                print(line[9], "leaves from", stopName, "in", willLeaveIn)
+                break
+            
+    
     scheduler.enter(30, 1, parseAndPrint, (sch,))
 
 
-scheduler.enter(30, 1, parseAndPrint, (scheduler,))
+scheduler.enter(1, 1, parseAndPrint, (scheduler,))
 scheduler.run()
